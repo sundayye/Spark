933250d6e80150bb11472ef949aeb9cef5ba0f29
allen <allenshow@foxmail.com>
2022-09-30 02:16:02 +0800

    FlowController code change. --An

--- src/main/java/com/ssc/ssgm/fx/ifx/integration/ui/controller/FlowController.java
+++ src/main/java/com/ssc/ssgm/fx/ifx/integration/ui/controller/FlowController.java
@@ -1,26 +1,21 @@
 
 package com.ssc.ssgm.fx.ifx.integration.ui.controller;
 
-import com.ssc.ssgm.fx.ifx.integration.api.HackthonContext;
 import com.ssc.ssgm.fx.ifx.integration.common.Response;
+import com.ssc.ssgm.fx.ifx.integration.core.config.*;
+import com.ssc.ssgm.fx.ifx.integration.core.flow.FlowContext;
 import com.ssc.ssgm.fx.ifx.integration.curd.service.FlowConfigService;
-import com.ssc.ssgm.fx.ifx.integration.curd.service.InboundConfigService;
-import com.ssc.ssgm.fx.ifx.integration.curd.service.KeyMapperConfigService;
-import com.ssc.ssgm.fx.ifx.integration.curd.service.OutboundConfigService;
 import com.ssc.ssgm.fx.ifx.integration.ui.dto.FlowDTO;
 import io.swagger.annotations.ApiOperation;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import com.ssc.ssgm.fx.ifx.integration.core.config.KeyMapperConfig;
 import com.ssc.ssgm.fx.ifx.integration.core.flow.FlowTransActionType;
 import com.ssc.ssgm.fx.ifx.integration.core.parser.ParserEnum;
 
@@ -33,25 +28,30 @@ public class FlowController {
     @Autowired
     FlowConfigService flowConfigService;
 
+    @Autowired
+    FlowContext flowContext;
 
     @ApiOperation("get_flow_types")
     @GetMapping("/get_flow_types")
-    public Response<List<String>> getFlowTypes() {
+    public Response<List<KeyValue>> getFlowTypes() {
         List<FlowTransActionType> typeEunmList = Arrays.asList(FlowTransActionType.values());
-        List<String> typeList = typeEunmList.stream().map(e ->{
-            return e.toString();
+        List<KeyValue> typeList = typeEunmList.stream().map(e ->{
+            KeyValue keyValue = new KeyValue();
+            keyValue.setLabel(e.toString());
+            keyValue.setName(e.toString());
+            return keyValue;
         } ).collect(Collectors.toList());
         return Response.success(typeList);
     }
 
     @ApiOperation("get_inbounds")
     @GetMapping("/get_inbounds")
-    public Response<?> getInbounds() {
-        Set<String>  inboundSet= HackthonContext.inboundMap.keySet();
-        List<KeyValue> inbounds = inboundSet.stream().map(e ->{
+    public Response<List<KeyValue>> getInbounds() {
+        List<InboundConfig> inboundConfigList = flowContext.getInboundConfigs();
+        List<KeyValue> inbounds = inboundConfigList.stream().map(e ->{
             KeyValue tKeyValue = new KeyValue();
-            tKeyValue.setLabel(e);
-            tKeyValue.setName(e);
+            tKeyValue.setLabel(e.getName());
+            tKeyValue.setName(e.getName());
             return tKeyValue;
         }).collect(Collectors.toList());
         return Response.success(inbounds);
@@ -59,37 +59,72 @@ public class FlowController {
 
     @ApiOperation("get_parsers")
     @GetMapping("/get_parsers")
-    public Response<?> getParsers() {
+    public Response<List<KeyValue>> getParsers() {
         List<ParserEnum> typeEunmList = Arrays.asList(ParserEnum.values());
-        List<String> typeList = typeEunmList.stream().map(e ->{
-            return e.toString();
+        List<KeyValue> typeList = typeEunmList.stream().map(e ->{
+            KeyValue keyValue = new KeyValue();
+            keyValue.setName(e.toString());
+            keyValue.setLabel(e.toString());
+            return keyValue;
         } ).collect(Collectors.toList());
         return Response.success(typeList);
     }
 
     @ApiOperation("get_key_mappers")
     @GetMapping("/get_key_mappers")
-    public Response<?> get_key_mappers() {
-
-        return Response.success();
+    public Response<List<KeyValue>> get_key_mappers() {
+        List<KeyMapperConfig> keyMapperConfigs = flowContext.getKeyMapperConfigs();
+        List<KeyValue> keyValues = keyMapperConfigs.stream().map(e ->{
+            KeyValue keyValue = new KeyValue();
+            keyValue.setName(e.getName());
+            keyValue.setLabel(e.getName());
+            return keyValue;
+        } ).collect(Collectors.toList());
+        return Response.success(keyValues);
     }
 
     @ApiOperation("get_formatters")
     @GetMapping("/get_formatters")
-    public Response<?> get_formatters() {
-        return Response.success();
+    public Response<List<KeyValue>> get_formatters() {
+        List<FormatterConfig> formatterConfigs = flowContext.getFormatterConfigs();
+        List<KeyValue> keyValues = formatterConfigs.stream().map(e ->{
+            KeyValue keyValue = new KeyValue();
+            keyValue.setName(e.getName());
+            keyValue.setLabel(e.getName());
+            return keyValue;
+        } ).collect(Collectors.toList());
+        return Response.success(keyValues);
     }
 
 
     @ApiOperation("get_outbounds")
     @GetMapping("/get_outbounds")
-    public Response<?> getOutbounds() {
-        return Response.success();
+    public Response<List<KeyValue>> getOutbounds() {
+        List<OutboundConfig> outboundConfigList = flowContext.getOutboundConfigs();
+        List<KeyValue> outbounds = outboundConfigList.stream().map(e ->{
+            KeyValue tKeyValue = new KeyValue();
+            tKeyValue.setLabel(e.getName());
+            tKeyValue.setName(e.getName());
+            return tKeyValue;
+        }).collect(Collectors.toList());
+        return Response.success(outbounds);
     }
 
     @ApiOperation("list")
     @GetMapping("/list")
-    public Response<?> list() {
+    public Response<List<FlowConfig>> list() {
+        flowContext.loadFlows();
+        List<FlowConfig> flowConfigs = flowContext.getFlowConfigs();
+        return Response.success(flowConfigs);
+    }
+
+    @ApiOperation("create")
+    @GetMapping("/create")
+    public Response<?> create(@RequestParam("flowConfig") FlowConfig flowConfig) {
+        if ( flowConfigService.addConfig(flowConfig) !=1){
+                return Response.fail();
+        }
+        flowContext.addFlowConfig(flowConfig);
         return Response.success();
     }
 
